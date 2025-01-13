56c12b0397fdbae20ac4555ffcc9662284b39600
Your Name <your.email@example.com>
2025-01-13 21:08:40 +0545

    Changed text of header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
         </div>
         <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I'm Namrata Shrestha  <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
-        <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Frontend Web Developer based in Kathmandu.</h1>
+        <h1 className='text-3xl sm:text-5xl lg:text-[60px] font-Ovo'>Frontend Web Developer</h1>
 
         <div className='flex flex-col md:flex-row items-center gap-4 mt-4'>
             <a href="#contact" 
