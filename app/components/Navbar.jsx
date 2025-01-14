26ee17caaa6b983f1a73b741ead0833e9533210b
Your Name <your.email@example.com>
2025-01-14 15:23:13 +0545

    Added dark theme

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { assets } from '@/assets/assets'
 import React, { useEffect, useRef, useState } from 'react'
 
-const Navbar = () => {
+const Navbar = ({isDarkMode, setIsDarkMode}) => {
     const [isScroll,setIsScroll]=useState(false)
 
     const sideMenuRef= useRef();
@@ -26,16 +26,16 @@ const Navbar = () => {
 
   return (
     <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80'>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80] dark:hidden'>
         <Image src={assets.header_bg_color} alt='' className='w-full'/>
     </div>
 
-        <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
+        <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : ""}`}>
             <a href='#top'>
-                <Image src={assets.logo} alt='logo' className='w-28 cursor-pointer mr-14'/>
+                <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='logo' className='w-28 cursor-pointer mr-14'/>
             </a>
 
-            <ul className={`hidden  md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : " bg-white shadow-sm bg-opacity-50"}`}>
+            <ul className={`hidden  md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : " bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"}`}>
                 <li><a className='font-Ovo' href='#top'>Home</a></li>
                 <li><a className='font-Ovo' href='#about'>About Me</a></li>
                 <li><a className='font-Ovo' href='#services'>Services</a></li>
@@ -44,8 +44,8 @@ const Navbar = () => {
             </ul>
 
             <div className='flex items-center gap-4'>
-                <button>
-                    <Image src={assets.moon_icon} alt='' className='w-6'/>
+                <button onClick={()=>setIsDarkMode(prev=> !prev)}>
+                    <Image src={isDarkMode ?assets.sun_icon : assets.moon_icon} alt='' className='w-6'/>
                 </button>
 
                 <a className='hidden md:flex items-center gap-2 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo' href="#contact">Contact
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -26,6 +26,7 @@ export default {
       }
     },
   },
+  darkMode:'selector',
   plugins: [],
 };
 
