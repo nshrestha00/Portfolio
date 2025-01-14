f9a018746acd857779b3c53e05b4716375a4a6f1
Your Name <your.email@example.com>
2025-01-14 16:01:57 +0545

    Added animation to image of header section

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,13 +1,18 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4 '>
-        <div>
+        <motion.div
+        initial={{scale:0}}
+        whileInView={{scale:1}} 
+        transition={{duration:0.8, type:'spring', stiffness:100}}
+        >
             <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
-        </div>
+        </motion.div>
         <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I'm Namrata Shrestha  <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
         <h1 className='text-3xl sm:text-5xl lg:text-[60px] font-Ovo'>Frontend Web Developer</h1>
 
