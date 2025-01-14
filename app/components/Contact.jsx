663df419e944cd693f1ad84e3e4105cf63abd688
Your Name <your.email@example.com>
2025-01-14 14:24:10 +0545

    Added web3forms in contact us page with email access_key

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,26 +3,51 @@ import Image from 'next/image'
 import React from 'react'
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "5c8a1a5e-bff6-4866-8d80-7c34f10e193a");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
       <h4 className='text-center mb-2 text-lg font-Ovo'> Connect with me</h4>
       <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I'd love to hear from you! If you have any questions, comments, or feedback, please use the form below.</p>
 
-      <form  className='max-w-2xl mx-auto'>
+      <form onSubmit={onSubmit}  className='max-w-2xl mx-auto'>
         <div className='grid grid-cols-auto gap-6 mt-10 mb-5'>
           <input type='text' placeholder='Enter your name' required
-          className='flex-1 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'/>
+          className='flex-1 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='name'/>
           <input type='email' placeholder='Enter your email' required
-          className='flex-1 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'/>
+          className='flex-1 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='email'/>
         </div>
         <textarea rows='6' placeholder="Enter your message" required 
-        className='w-full y-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6'></textarea>
+        className='w-full y-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' name='message'></textarea>
         <button type='submit' className='py-2 px-2 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'>Submit now
           <Image src={assets.right_arrow_white} alt="" className='w-4'/>
         </button>
 
-        <p className='mt-4'>Sending....</p>
+        <p className='mt-4'>{result}</p>
       </form>
 
     </div>
