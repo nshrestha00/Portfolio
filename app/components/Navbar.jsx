7442a68ba94ce9b93874d5ac88fa2f3631b778e0
Your Name <your.email@example.com>
2025-01-14 15:28:14 +0545

    Added dark mode in mobile menu for navabar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -58,9 +58,9 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
             </div>
 
             {/* Mobile Menu */}
-            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white'>
                 <div className='absolute right-6 top-6' onClick={closeMenu}>
-                    <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+                    <Image src={isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer'/>
                 </div>
                 <li><a className='font-Ovo' onClick={closeMenu}  href='#top'>Home</a></li>
                 <li><a className='font-Ovo' onClick={closeMenu}  href='#about'>About Me</a></li>
