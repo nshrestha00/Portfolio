e66128f3e639e8d38c5f0f5edb31ae2385f02f32
Your Name <your.email@example.com>
2025-01-15 12:28:07 +0545

    Changed the work section

--- assets/assets.js
+++ assets/assets.js
@@ -76,24 +76,14 @@ export const assets = {
 
 export const workData = [
     {
-        title: 'Frontend project',
-        description: 'Web Design',
+        title: 'Mern stack project',
+        description: 'Online Clothing Store',
         bgImage: '/work-1.png',
     },
     {
-        title: 'Geo based app',
-        description: 'Mobile App',
+        title: 'Mern stack project',
+        description: 'Food Delivery Website',
         bgImage: '/work-2.png',
-    },
-    {
-        title: 'Photography site',
-        description: 'Web Design',
-        bgImage: '/work-3.png',
-    },
-    {
-        title: 'UI/UX designing',
-        description: 'UI/UX Design',
-        bgImage: '/work-4.png',
     },
 ]
 
